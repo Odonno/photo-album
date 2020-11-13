4148c4b48c2b6af46de5f27655d8a15681e9b0a7
David Bottiau <bottiau.david@laposte.net>
2020-11-13 10:09:13 +0100

    chore: update project to be able to deploy to Azure

--- photo-album-front/next.config.js
+++ photo-album-front/next.config.js
@@ -2,8 +2,11 @@ module.exports = {
 	images: {
 		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
 		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-		domains: ['<YOUR_AZURE_BLOB_STORAGE_DOMAIN>'],
+		domains: ['<YOUR_AZURE_BLOB_STORAGE_DOMAIN>'], // ex: saphotoalbum.blob.core.windows.net
 		path: '/_next/image',
 		loader: 'default',
+	},
+	env: {
+		PHOTO_ALBUM_API_URL: '<YOUR_BACKEND_URL>', // ex: https://localhost:5001
 	},
 };
--- photo-album-front/pages/index.tsx
+++ photo-album-front/pages/index.tsx
@@ -32,6 +32,7 @@ export const getStaticProps = async () => {
 		props: {
 			photos,
 		},
+		revalidate: 1,
 	};
 };
 
